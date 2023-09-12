9a82f6ca2abd3ec39822276e7eac60308664f6b6
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-09-12 18:58:46 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -32,8 +32,8 @@ fi
 sudo chown -R ubuntu:ubuntu /data/
 
 if ! cat < /etc/nginx/sites-enabled/default | grep -q "uceetheblackone.tech"; then
-    sudo sed -i '0,/^\(\s*\)server_name\s*.*$/s//\1server_name olisabelema.tech www.michaeldecent.tech;/' /etc/nginx/sites-available/default
-    sudo sed -i '0,/^\(\s*\)server_name olisabelema.tech www.michaeldecent.tech;$/s//&\n\n\1location \/hbnb_static {\n\1\1alias \/data\/web_static\/current\/;\n\1\1autoindex off;\n\1}/' /etc/nginx/sites-available/default
+    sudo sed -i '0,/^\(\s*\)server_name\s*.*$/s//\1server_name uceetheblackone.tech www.michaeldecent.tech;/' /etc/nginx/sites-available/default
+    sudo sed -i '0,/^\(\s*\)server_name uceetheblackone.tech www.uceetheblackone.tech;$/s//&\n\n\1location \/hbnb_static {\n\1\1alias \/data\/web_static\/current\/;\n\1\1autoindex off;\n\1}/' /etc/nginx/sites-available/default
 fi
 
 # Restarting Nginx
