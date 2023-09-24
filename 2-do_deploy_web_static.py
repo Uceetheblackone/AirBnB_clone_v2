44f8e8fd9d8341add4ccefdd0ed3956597e72dde
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-09-24 13:16:56 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,46 @@
- Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy
+#!/usr/bin/python3
+"""
+Fabric script to deploy tgz archive
+fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
+    -i private-key -u user
 """
 
-
-from fabric.api import *
 from os.path import exists
-from os import getenv, environ
+from fabric.api import put, run, env
 
-env.hosts = ['54.236.33.100', '100.26.252.11']
-env.user = 'ubuntu'
-env.key_filename = '/home/root/.ssh/id_rsa'
+env.hosts = ['35.243.128.200', '3.239.120.96']
 
 
 def do_deploy(archive_path):
-    """Deploys the web static to the server"""
+    """
+    copies archive file from local to my webservers
+    """
+
     if not exists(archive_path):
-        print("path does not exist\n")
         return False
-
     try:
-        archive_name = archive_path.split('/')[-1]
-        file_name = archive_name.split('.')[0]
-        sym_link = "/data/web_static/current"
-        release_version = f"/data/web_static/releases/{file_name}/"
+        file_name = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp/")
 
-        # deploying locally
-        run_locally = getenv("run_locally", None)
-        if run_locally is None:
-            print(f"Deploying new_version from {archive_path}")
-            local(f"sudo mkdir -p {release_version}")
-            local(f"sudo tar -xzf {archive_path} \
--C {release_version} --strip-components=1")
-            local(f"sudo rm -f {sym_link}")
-            local(f"sudo ln -s {release_version} {sym_link}")
-            environ['run_locally'] = "True"
-            print("Deployed locally\n")
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
 
-        put(archive_path, f"/tmp/{archive_name}")
-        run(f"mkdir -p {release_version}")
-        run(f"tar -xzf /tmp/{archive_name} \
--C {release_version} --strip-components=1")
-        run(f"rm /tmp/{archive_name}")
-        run(f"rm -f {sym_link}")
-        run(f"ln -s {release_version} {sym_link}")
-        print(f"New Version Deployed --> {release_version}")
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
+
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
+
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
+            .format(file_name, file_name))
+
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
+
+        run('rm -rf /data/web_static/current')
+
+        run(('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current')
+            .format(file_name))
         return True
-    except Exception as e:
-        print(f"Failed to Deploy New Version --> {release_version}\n{str(e)}")
+    except Exception:
         return False
